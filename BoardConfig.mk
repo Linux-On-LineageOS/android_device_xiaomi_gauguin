f651a629b5750940f3bfd0f9a75b6608cce27e3d
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:41:09 +0800

    Disable SELinux

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,3 +187,4 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
 include vendor/xiaomi/gauguin/BoardConfigVendor.mk
+BOARD_BOOTCONFIG += androidboot.selinux=permissive
